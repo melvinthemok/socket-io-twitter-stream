0bab8bae040300d5d6277b8cd398eb2838605a60
melvinthemok <melvinmok.cy@gmail.com>
2017-02-28 10:58:29 +0800

    Complete exercise

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
-var morgan  = require('morgan');
-var express = require('express');
-var app     = express();
-var port    = process.env.PORT || 3000;
-var router  = express.Router();
+var morgan = require('morgan')
+var express = require('express')
+var app = express()
+var http = require('http').createServer(app)
+require('dotenv').config()
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
+var port = process.env.PORT || 3000
+var router = express.Router()
 
-app.use(morgan('dev'));
+var io = require('socket.io')(http)
+var Twit = require('twit')
+
+var twitter = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
+
+io.on('connect', function (socket) {
+  var stream = twitter.stream('statuses/filter', { track: 'moonlight' })
+
+  stream.on('tweet', function (tweet) {
+    var data = {}
+    data.name = tweet.user.name
+    data.screen_name = tweet.user.screen_name
+    data.text = tweet.text
+    data.user_profile_image = tweet.user.profile_image_url
+    socket.emit('tweets', data)
+  })
+})
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+app.use(morgan('dev'))
 
 // app.use(function(req, res, next) {
 //   console.log('%s request to %s from %s', req.method, req.path, req.ip);
@@ -18,19 +45,22 @@ app.use(morgan('dev'));
 //     res.render('index');
 // });
 
-router.get('/', function(req, res) {
-  res.render('index', { header: 'Twitter Search'});
-});
+// unnecessary use of a router...
 
-router.get('/contact', function(req, res) {
-  res.render('contact', { header: 'contact!'});
-});
+router.get('/', function (req, res) {
+  res.render('index', { header: 'Twitter Search'})
+})
 
-router.get('/about', function(req, res) {
-  res.render('about', { header: 'about!'});
-});
+router.get('/contact', function (req, res) {
+  res.render('contact', { header: 'contact!'})
+})
 
-app.use('/', router);
-app.listen(port);
+router.get('/about', function (req, res) {
+  res.render('about', { header: 'about!'})
+})
 
-console.log('Server started on ' + port);
+app.use('/', router)
+
+http.listen(port)
+
+console.log('Server started on ' + port)
